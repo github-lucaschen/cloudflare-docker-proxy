ec4c97de885eb12adb6df841324d490b4fd4bb8a
lucas <107790059+github-lucaschen@users.noreply.github.com>
2024-07-23 14:06:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.lucas-chen.com": dockerHub,
+  "quay.lucas-chen.com": "https://quay.io",
+  "gcr.lucas-chen.com": "https://gcr.io",
+  "k8s-gcr.lucas-chen.com": "https://k8s.gcr.io",
+  "k8s.lucas-chen.com": "https://registry.k8s.io",
+  "ghcr.lucas-chen.com": "https://ghcr.io",
+  "cloudsmith.lucas-chen.com": "https://docker.cloudsmith.io",
+  "ecr.lucas-chen.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.lucas-chen.com": dockerHub,
 };
 
 function routeByHosts(host) {
